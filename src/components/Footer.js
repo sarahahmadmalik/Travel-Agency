5fcd625411ffa8f26fa5254944d3b9f72ccd47c5
Sarah Ahmad Malik <sarahahmadmalik@gmail.com>
2023-04-08 16:53:16 +0500

    updated

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -1,50 +1,49 @@
-import '../styles/Footer.css';
-
-const Footer = () =>{
-    return(
-        <>
-    <div className="footer">
-    <div className="top">
-    <div>
-    <h1>Traveled</h1>
-    </div>
-    <div></div>
-    </div>
-    <div className="bottom">
-    <div>
-<h4>Project</h4>
-<a href="/">Changelog</a>
-<a href="/">Status</a>
-<a href="/">License</a>
-<a href="/">All Versions</a>
-</div>
-<div>
-<h4>Community</h4>
-<a href="/">GitHub</a>
-<a href="/">Issues</a>
-<a href="/">Project</a>
-<a href="/"> Twitter</a>
-</div>
-<div>
-<h4>Help</h4>
-<a href="/">Support</a>
-<a href="/">Troubleshooting</a>
-<a href="/">Contact Us</a>
-</div>
-<div>
-<h4>Others</h4>
-<a href="/">Terms of Service</a>
-<a href="/">Privacy Policy</a>
-<a href="/">License</a>
-</div>
-    </div>
-    </div>
-    <div className='credits'>
-        <p >Made by Sara Ahmad</p>
+import "../styles/Footer.css";
+import {Link} from 'react-router-dom'
+const Footer = () => {
+  return (
+    <>
+      <div className="footer">
+        <div className="top">
+          <div>
+            <h1 style={{cursor: "pointer"}}>Traveled</h1>
+          </div>
+          <div></div>
+        </div>
+        <div className="bottom">
+          <div>
+            <h4>Project</h4>
+            <Link href="/">Changelog</Link>
+            <Link href="/">Status</Link>
+            <Link href="/">License</Link>
+            <Link href="/">All Versions</Link>
+          </div>
+          <div>
+            <h4>Community</h4>
+            <Link href="/">GitHub</Link>
+            <Link href="/">Issues</Link>
+            <Link href="/">Project</Link>
+            <Link href="/"> Twitter</Link>
+          </div>
+          <div>
+            <h4>Help</h4>
+            <Link href="/">Support</Link>
+            <Link href="/">Troubleshooting</Link>
+            <Link href="/">Contact Us</Link>
+          </div>
+          <div>
+            <h4>Others</h4>
+            <Link href="/">Terms of Service</Link>
+            <Link href="/">Privacy Policy</Link>
+            <Link href="/">License</Link>
+          </div>
+        </div>
+      </div>
+      <div className="credits">
+        <p>Made by Sara Ahmad</p>
         <p>All rights reserved</p>
-
-    </div>
+      </div>
     </>
-    )
-}
+  );
+};
 export default Footer;
