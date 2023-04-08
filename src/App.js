5f56924ab454a1f15faa7f29f6db4d25f6939ac0
Sarah Ahmad Malik <sarahahmadmalik@gmail.com>
2023-04-08 18:56:11 +0500

    deploy

--- src/App.js
+++ src/App.js
@@ -2,7 +2,7 @@ import { BrowserRouter, Route, Routes} from 'react-router-dom';
 import Destinations from './Destinations';
 import Contact from './Contact'
 import Home from './Home';
-import About from './pages/About';
+import About from './About';
 
 function App() {
   return (
