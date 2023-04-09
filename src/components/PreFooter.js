dba7de3fd02ccbf3533a9af96204fd97aa71d6d4
Sarah Ahmad Malik <sarahahmadmalik@gmail.com>
2023-04-09 15:05:23 +0500

    css changes

--- src/components/PreFooter.js
+++ src/components/PreFooter.js
@@ -6,7 +6,7 @@ function PreFooter() {
         <p>Want to Learn More?</p>
         <p>We’re here to help whether you need information or you already have a travel program</p>
         <div className='contact'>
-            <button>WhatsApp us</button><span>or</span><button>Email</button>
+            <button id='footer-btn'>WhatsApp us</button><span>or</span><button id='footer-btn'>Email</button>
         </div>
     </div>
   )
